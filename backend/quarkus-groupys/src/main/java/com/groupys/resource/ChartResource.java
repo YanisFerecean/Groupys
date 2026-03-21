90fdd5624adab6ab5cb4b8b2b977f79ab6394928
KyawIT <kyawhtethtun65@gmail.com>
2026-03-21 02:27:02 +0100

    feat(backend): add chart resource

---
+++ backend/quarkus-groupys/src/main/java/com/groupys/resource/ChartResource.java
@@ -0,0 +1,62 @@
+package com.groupys.resource;
+
+import com.groupys.dto.ArtistResDto;
+import com.groupys.dto.TopAlbumResDto;
+import com.groupys.dto.TopTrackResDto;
+import com.groupys.service.ChartService;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.DefaultValue;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+
+@Path("/charts")
+@Produces(MediaType.APPLICATION_JSON)
+public class ChartResource {
+
+    @Inject
+    ChartService chartService;
+
+    @GET
+    @Path("/tracks/global")
+    public List<TopTrackResDto> getGlobalTopTracks() {
+        return chartService.getGlobalTopTracks();
+    }
+
+    @GET
+    @Path("/tracks/country")
+    public List<TopTrackResDto> getTopTracksByCountry(
+            @DefaultValue("United States") @QueryParam("country") String country) {
+        return chartService.getTopTracksByCountry(country);
+    }
+
+    @GET
+    @Path("/artists/global")
+    public List<ArtistResDto> getGlobalTopArtists() {
+        return chartService.getGlobalTopArtists();
+    }
+
+    @GET
+    @Path("/artists/country")
+    public List<ArtistResDto> getTopArtistsByCountry(
+            @DefaultValue("United States") @QueryParam("country") String country) {
+        return chartService.getTopArtistsByCountry(country);
+    }
+
+    @GET
+    @Path("/albums/global")
+    public List<TopAlbumResDto> getGlobalTopAlbums() {
+        return chartService.getGlobalTopAlbums();
+    }
+
+    @GET
+    @Path("/albums")
+    public List<TopAlbumResDto> getTopAlbumsByTag(
+            @DefaultValue("pop") @QueryParam("tag") String tag) {
+        return chartService.getTopAlbumsByTag(tag);
+    }
+}
