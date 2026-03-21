9e001f49b1e4a9e735ef6c7c328d84e9523b3e50
KyawIT <kyawhtethtun65@gmail.com>
2026-03-21 16:46:55 +0100

    feat(mobile): add Clerk provider to root layout

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,21 @@
-import { Stack } from "expo-router";
-import '@/global.css';
+import '@/global.css'
+import { ClerkProvider } from '@clerk/expo'
+import { tokenCache } from '@clerk/expo/token-cache'
+import { Slot } from 'expo-router'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+if (!publishableKey) {
+  throw new Error('Add EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY to your .env file')
+}
+
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+        <Slot />
+      </ClerkProvider>
+    </SafeAreaProvider>
+  )
 }
