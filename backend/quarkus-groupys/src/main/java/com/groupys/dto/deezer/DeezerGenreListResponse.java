905a2f804622dc1a139b414fb9fbced3ad89ac01
KyawIT <kyawhtethtun65@gmail.com>
2026-03-24 00:07:49 +0100

    chore(backend): update DeezerGenreListResponse.java

---
+++ backend/quarkus-groupys/src/main/java/com/groupys/dto/deezer/DeezerGenreListResponse.java
@@ -0,0 +1,11 @@
+package com.groupys.dto.deezer;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DeezerGenreListResponse(
+        List<DeezerGenreDto> data
+) {
+}
