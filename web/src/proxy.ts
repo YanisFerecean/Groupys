08264ecd16c357e7302eb8902fecd9e793210d37
KyawIT <kyawhtethtun65@gmail.com>
2026-03-24 15:35:48 +0100

    fix(web): proxy config

--- web/src/proxy.ts
+++ web/src/proxy.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
 
-const ALLOWED_PREFIXES = ["/", "/api", "/_next", "/favicon.ico", "/blog", "/coming-soon", "/privacy", "/og-image"];
+const ALLOWED_PREFIXES = ["/", "/api", "/_next", "/favicon.ico", "/blog", "/coming-soon", "/privacy", "/og-image", "/sitemap.xml", "/sitemap-0.xml", "/robots.txt"];
 
 export const proxy = clerkMiddleware((_, request: NextRequest) => {
   if (process.env.APP_ENV === "prod") {
@@ -19,7 +19,7 @@ export const proxy = clerkMiddleware((_, request: NextRequest) => {
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|xml|webmanifest)).*)",
     // Always run for API routes
     "/(api|trpc)(.*)",
   ],
