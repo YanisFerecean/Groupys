6397c97d1f0a04326d137f07d63167051b2677e1
KyawIT <kyawhtethtun65@gmail.com>
2026-03-28 22:19:23 +0100

    feat(backend): add MatchResource.java

---
+++ backend/quarkus-groupys/src/main/java/com/groupys/resource/MatchResource.java
@@ -0,0 +1,46 @@
+package com.groupys.resource;
+
+import com.groupys.dto.MatchResDto;
+import com.groupys.service.MatchService;
+import io.quarkus.security.Authenticated;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.jwt.JsonWebToken;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
+
+import java.util.List;
+import java.util.UUID;
+
+@Path("/matches")
+@Authenticated
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@SecurityRequirement(name = "bearerAuth")
+public class MatchResource {
+
+    @Inject
+    MatchService matchService;
+
+    @Inject
+    JsonWebToken jwt;
+
+    @GET
+    public List<MatchResDto> getMatches() {
+        return matchService.getMatches(jwt.getSubject());
+    }
+
+    @GET
+    @Path("/{matchId}")
+    public MatchResDto getMatch(@PathParam("matchId") UUID matchId) {
+        return matchService.getMatch(jwt.getSubject(), matchId);
+    }
+
+    @DELETE
+    @Path("/{matchId}")
+    public Response unmatch(@PathParam("matchId") UUID matchId) {
+        matchService.unmatch(jwt.getSubject(), matchId);
+        return Response.noContent().build();
+    }
+}
