ace0e054b7d81e7667e244e7a244461b1f996431
KyawIT <kyawhtethtun65@gmail.com>
2026-03-28 22:19:21 +0100

    chore(backend): update TrackRepository.java

--- backend/quarkus-groupys/src/main/java/com/groupys/repository/TrackRepository.java
+++ backend/quarkus-groupys/src/main/java/com/groupys/repository/TrackRepository.java
@@ -4,6 +4,12 @@ import com.groupys.model.Track;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.util.Optional;
+
 @ApplicationScoped
 public class TrackRepository implements PanacheRepositoryBase<Track, Long> {
+
+    public Optional<Track> findBySpotifyId(String spotifyId) {
+        return find("spotifyId", spotifyId).firstResultOptional();
+    }
 }
